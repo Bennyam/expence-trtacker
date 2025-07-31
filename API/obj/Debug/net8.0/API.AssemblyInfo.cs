95b64681fd7e5f5cd727e96fb1fab7c3a6dc6ba6
Bennyam <38783044+Bennyam@users.noreply.github.com>
2025-07-31 18:41:29 +0200

    Cloned to desktop

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad0683c97371f963c5c0f0ec6e26668791ca4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f1e97e2c2b680496a74f714e09d5232fdc1c59")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
