24faf31a221edbfa16bd63c5c91cba54ba4a7286
Bennyam <ben.ameryckx@icloud.com>
2025-08-01 10:51:15 +0200

    changes in database

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe8a0d7ae69810a8187b6eca84b5620c93074d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed72e092ae225ac6232938d2bf6d67f9b609ba7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
